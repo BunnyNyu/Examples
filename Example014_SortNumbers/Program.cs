974a3e87a483dc12062860fa391c6de7849b7894
Funny-Bunny <work.nyu@gmail.com>
2023-03-19 00:59:04 +0300

    Add Example 014 Sorted Numbers project

---
+++ Example014_SortNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿//Сортировка чисел
+
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array) // 1 способ (выводит содержимое массива на экран)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} "); // вывод в одну строку
+    }
+    Console.WriteLine(); // пустая строка
+}
+
+void SelectionSort(int[] array) // сортирует массив
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
